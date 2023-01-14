c630fdfefcec5dfad44b16736349e283caa06b8d
slowtahoe <111470320+slowtahoe@users.noreply.github.com>
2023-01-14 03:14:26 -0500

    atm changes
    added blb

--- GameMode_CityRPG4/server/brickScripts/info/atm.cs
+++ GameMode_CityRPG4/server/brickScripts/info/atm.cs
@@ -1,8 +1,11 @@
 // ============================================================
 // Brick Data
 // ============================================================
-datablock fxDTSBrickData(CityRPGATMBrickData)
+datablock fxDTSBrickData(CityRPGATMBrickData : brick2x4FData)
 {
+	brickFile = $City::DataPath @ "bricks/atm.blb";
+	iconName = $City::DataPath @ "ui/BrickIcons/ATM";
+
 	category = "CityRPG";
 	subCategory = "Info Bricks";
 
@@ -14,49 +17,22 @@ datablock fxDTSBrickData(CityRPGATMBrickData)
 	triggerDatablock = CityRPGInputTriggerData;
 	triggerSize = "4 4 3";
 	trigger = 0;
-
-	iconName = "add-ons/GameMode_CityRPG4/data/ui/BrickIcons/ATM";
-
-	brickSizeX=2;
-	brickSizeY=3;
-	brickSizeZ=10;
-
-	//Brick
-	brickRender=0;
-	brickCollide=1;
-	brickRaycast=1;
-	
-	//Model
-	spawnModel="add-ons/GameMode_CityRPG4/data/shapes/atm.1.dts";
-	modelOffset="0 0 0";
-	modelScale="1 1 1";
-	
-	colorCount=2;
-	
-	colorGroup[0]="ALL";
-		colorMode[0]="Intensity";
-		colorShift[0]="1 1 1 1";
-
-	colorGroup[1]="Screen";
-		colorMode[1]="Fix";
-		colorShift[1]="0 0.7 0 1";
 };
 
 // ============================================================
 // Menu
 // ============================================================
-$City::Menu::ATMBaseTxt = "Withdraw money.";
-
-// We can call directly on the same prompt that the bank uses.
-$City::Menu::ATMBaseFunc = "CityMenu_BankWithdrawPrompt";
-
 function CityMenu_ATM(%client, %brick)
 {
-	%client.cityMenuMessage("\c6You have " @ $c_p @ "$" @ City.get(%client.bl_id, "bank") SPC "\c6in your account.");
+	%client.cityMenuMessage("\c6You have \c3$" @ City.get(%client.bl_id, "bank") SPC "\c6in your account.");
 
 	%client.cityLog("Enter ATM");
 
-	%client.cityMenuOpen($City::Menu::ATMBaseTxt, $City::Menu::ATMBaseFunc, %brick, "\c6Thanks, come again.", 0, 0, $c_p @ "ATM");
+	%menu = "Withdraw money";
+	// We can call directly on the same prompt that the bank uses.
+	%functions = "CityMenu_BankWithdrawPrompt";
+
+	%client.cityMenuOpen(%menu, %functions, %brick, "\c6Thanks, come again.", 0, 0, "\c3ATM");
 }
 
 // ============================================================
@@ -66,7 +42,7 @@ function CityRPGATMBrickData::parseData(%this, %brick, %client, %triggerStatus, 
 {
 	if(%triggerStatus == true && !%client.cityMenuOpen)
 	{
-		%client.cityMenuMessage("\c6Welcome to the " @ $Pref::Server::City::name @ " Bank. Your account balance is " @ $c_p @ "$" @ City.get(%client.bl_id, "bank") @ "\c6. Current economy value: " @ $c_p @ %econColor @ $City::Economics::Condition @ "\c6%");
+		%client.cityMenuMessage("\c6Welcome to the " @ $Pref::Server::City::name @ " Bank. Your account balance is \c3$" @ City.get(%client.bl_id, "bank") @ "\c6. Current economy value: \c3" @ %econColor @ $City::Economics::Condition @ "\c6%");
 
 		CityMenu_ATM(%client, %brick);
 	}
