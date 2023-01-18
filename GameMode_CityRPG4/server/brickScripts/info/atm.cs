e7e1181661734d3bc7c63dba57ea4eefb6b0bad0
slowtahoe <111470320+slowtahoe@users.noreply.github.com>
2023-01-17 21:19:57 -0500

    Update atm.cs
    atm hacking added

--- GameMode_CityRPG4/server/brickScripts/info/atm.cs
+++ GameMode_CityRPG4/server/brickScripts/info/atm.cs
@@ -22,17 +22,95 @@ datablock fxDTSBrickData(CityRPGATMBrickData)
 // ============================================================
 // Menu
 // ============================================================
+$City::Menu::ATMBaseTxt =	
+		"Withdraw money."
+	TAB "Hack ATM.";
+
+$City::Menu::ATMBaseFunc =	
+		"CityMenu_BankWithdrawPrompt"
+	TAB "CityMenu_HackATMPrompt";
+
 function CityMenu_ATM(%client, %brick)
 {
 	%client.cityMenuMessage("\c6You have \c3$" @ City.get(%client.bl_id, "bank") SPC "\c6in your account.");
 
 	%client.cityLog("Enter ATM");
 
-	%menu = "Withdraw money";
+	%menu = $City::Menu::ATMBaseTxt;
 	// We can call directly on the same prompt that the bank uses.
-	%functions = "CityMenu_BankWithdrawPrompt";
+	%functions = $City::Menu::ATMBaseFunc;
 
 	%client.cityMenuOpen(%menu, %functions, %brick, "\c6Thanks, come again.", 0, 0, "\c3ATM");
+}
+
+function CityMenu_HackATMPrompt(%client)
+{
+	for(%i = 0; %i < ClientGroup.getCount(); %i++)
+	{
+		%clientCheck = ClientGroup.getObject(%i);
+		if((%clientCheck.getJobSO().law) == true)
+		{
+			%disableHacking = false;
+		}
+		else
+		{
+			%disableHacking = true;
+		}
+	}
+
+	if(%disableHacking == true)
+	{
+		messageClient(%client,'',"\c6No law enforcement online. ATM hacking is disabled.");
+		return;
+	}
+
+	if(%client.AccessableATM == 1) 
+	{
+		messageClient(%client,'',"\c6You need to wait a while before hacking again. 7sec from the last.");
+		return;
+	}
+	else
+	{
+		if(CityRPGData.getData(%client.bl_id).valueEducation >= 3)
+		{
+			%stealchance = getRandom(1,2);
+			%caughtchance = getRandom(1,4);
+			%lockoutchance = getRandom(1,3);
+			%beencaught = 1;
+			if(%stealchance == 1)
+			{
+				if(%lockoutchance != 1)
+				{
+					%stolen = getRandom($ATM::Min,$ATM::Max);
+					messageClient(%client,'',"\c6You managed to steal \c3$" @ %stolen @ "\c6 from the ATM.");
+					CityRPGData.getData(%client.bl_id).valueMoney += %stolen;
+					%client.AccessableATM = 1;
+					CityRPGData.getData(%client.bl_id).valueDemerits += $ATM::Demerits;
+					schedule(7000,0,"resetAccessableATM",%client);
+					if(%caughtchance != 1)
+					{
+						messageAll('',"\c3" @ %client.name @ "\c6 has been caught hacking an ATM!");
+						CityRPGData.getData(%client.bl_id).valueDemerits += $ATM::Demerits;
+					}
+				}
+				else
+				{
+					%this.hackable = 0;
+					messageClient(%client,'',"\c6Your attempt failed, and you have been locked out of the machine.");
+					
+				}
+			}
+			else
+			{
+				messageClient(%client,'',"\c6Failed to hack.");
+			}
+		}
+		else
+		{
+			messageClient(%client,'',"\c6Your education level must be \c3" @ $Pref::Server::City::hack::education @ "\c6.");
+		}
+		return;
+	}
 }
 
 // ============================================================
@@ -46,4 +124,9 @@ function CityRPGATMBrickData::parseData(%this, %brick, %client, %triggerStatus, 
 
 		CityMenu_ATM(%client, %brick);
 	}
+}
+
+function resetAccessableATM(%client)
+{
+	%client.AccessableATM = 0;
 }
